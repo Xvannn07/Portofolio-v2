ad50d152d9c1f79cb171f6da58104a799a7e4305
Xvannn 文 <95283773+Xvannn07@users.noreply.github.com>
2024-01-08 12:18:13 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -108,6 +108,10 @@ export default {
             I just want to need the right calm, and want to focus on the outside world so I can achieve what I 
             want like friends and partners
           </p>
+          <p class="mb-3 md:mb-7 fadein-left fadeins-2">
+            &nbsp; &nbsp; &nbsp; thanks to Bagas Rakha (template maker),
+            I just changed some code name
+          </p>
         </div>
 
       </section>
